b58817234bb03d4acc6857b26eebfbfe18e4fd0e
João M. Bezerra <marcospb19@hotmail.com>
2020-12-08 13:52:19 -0300

    Add bspwm polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 killall -q polybar
-while pgrep -u $UID -x polybar > /dev/null; do sleep 0.2; done
+pkill polybar
+while pgrep -u $UID -x polybar > /dev/null; do sleep '0.1'; done
 
 polybar -q HDMI &
-sleep 0.15 # Making sure that the first runs first
-polybar -q eDP &
+# sleep 0.15 # Making sure that the first runs first
+# polybar -q eDP &
