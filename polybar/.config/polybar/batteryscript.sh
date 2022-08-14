eda05550e5a2ca0678e3c28a9df610097f6e07a9
João M. Bezerra <marcospb19@hotmail.com>
2022-08-14 14:48:38 -0300

    fix polybar batteryscript

--- polybar/.config/polybar/batteryscript.sh
+++ polybar/.config/polybar/batteryscript.sh
@@ -1,2 +1,2 @@
-level=$(cat /sys/class/power_supply/BAT1/capacity)
+level=$(cat /sys/class/power_supply/BAT0/capacity)
 [ "${level}" -le 25 ] && printf "Low battery: ${level}%%" || ( [ "${level}" -le 95 ] && printf "${level}%%" )
