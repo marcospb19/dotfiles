fac8ac17c3524639c37e69e0ab34d4f24357f298
João M. Bezerra <marcospb19@hotmail.com>
2023-07-11 18:27:39 -0300

    update polybar

--- polybar/.config/polybar/volumeismutedscript.sh
+++
@@ -1,5 +0,0 @@
-if [ "$(pamixer --get-volume-human)" = 'muted' ]; then
-    echo '  muted  '
-else
-    echo
-fi
--- polybar/.config/polybar/volumescript.sh
+++ polybar/.config/polybar/volumescript.sh
@@ -1,1 +1,7 @@
-echo "$(pamixer --get-volume | tr -d '\n')%"
+output="$(pamixer --get-volume | tr -d '\n')%"
+
+if [ "$(pamixer --get-volume-human)" = 'muted' ]; then
+    output="${output} (muted)"
+fi
+
+echo $output
