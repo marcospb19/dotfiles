1a7278f82bd2f9b6ea5e502711c201fb9650fde7
João Marcos P. Bezerra <marcospb19@hotmail.com>
2023-10-22 15:01:43 -0300

    use 75 Hz monitor refresh rate

--- screenlayout/.screenlayout/settings.sh
+++ screenlayout/.screenlayout/settings.sh
@@ -1,7 +1,7 @@
 notebook_display="eDP-1"
 notebook_display_resolution="1920x1080"
 
-external_display="HDMI-1"
+external_display="HDMI-A-0"
 external_display_resolution="2560x1080"
 
 # Same width as $notebook_display_resolution
