1b04430f8d4bae7bf1fb1cb83a557ca5590690f9
João Marcos P. Bezerra <marcospb19@hotmail.com>
2024-06-15 17:05:51 -0300

    update polybar for new monitor

--- screenlayout/.screenlayout/external.sh
+++ screenlayout/.screenlayout/external.sh
@@ -4,4 +4,4 @@ source ~/.screenlayout/settings.sh
 
 xrandr \
 	--output $notebook_display --off \
-	--output $external_display --mode $external_display_resolution --refresh 74.99 
+	--output $external_display --mode $external_display_resolution --refresh 200
--- screenlayout/.screenlayout/settings.sh
+++ screenlayout/.screenlayout/settings.sh
@@ -1,8 +1,8 @@
 notebook_display="eDP-1"
 notebook_display_resolution="1920x1080"
 
-external_display="HDMI-A-0"
-external_display_resolution="2560x1080"
+external_display="DisplayPort-2"
+external_display_resolution="3440x1440"
 
 # Same width as $notebook_display_resolution
-second_display_offset="2560x0"
+second_display_offset="3440x0"
