bb2fc1339efc6068bc4328d80a630d60fd9f3c69
João Marcos P. Bezerra <marcospb19@hotmail.com>
2024-11-29 21:14:40 -0300

    add Glava config

---
+++ glava/.config/glava/env_KWin.glsl
@@ -0,0 +1,7 @@
+#request setdecorated false
+#request setxwintype "normal"
+#request addxwinstate "below"
+#request addxwinstate "skip_taskbar"
+#request addxwinstate "skip_pager"
+#request addxwinstate "pinned"
+#request setclickthrough true
