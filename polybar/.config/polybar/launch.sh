a127c5282a508823e2d2cf4e4ba0ab8abb2ddaa7
João Marcos <marcospb19@hotmail.com>
2025-02-12 15:45:41 -0300

    move some files to the polybar group

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,6 +4,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 0.05; done
 
 # polybar -q DisplayPort &
-polybar -q HDMI &
+# polybar -q HDMI &
 # sleep 0.15 # Making sure that the first runs first
-# polybar -q eDP &
+polybar -q DisplayPort &
